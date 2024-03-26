dcb43dcb0fd4de2dee70a9a481a6b410c9013141
SoloCoding <zx-kiki@hanmail.net>
2024-03-27 08:23:03 +0900

    3일차 upload
    upload

---
+++ DAY3/1_컨테이너의공통의특징.cpp
@@ -0,0 +1,20 @@
+﻿// STL 컨테이너의 특징
+#include <vector>
+#include <iostream>
+#include "show.h"
+
+int main()
+{
+	std::vector<int> v = { 1,2,3 };
+
+	// 1. 멤버 타입이 있다.
+	? sz = s.size();
+    ? cnt = v.end() - v.begin();
+
+	// 2. 반환과 제거를 동시에 하지 않는다.
+	int n = v.pop_back();	// ?
+
+
+	// 3. 단위 전략을 많이 사용한다.
+	std::vector<int, debug_alloc<int>> v2; // ?
+}
