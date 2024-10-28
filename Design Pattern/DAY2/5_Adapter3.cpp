435cd75be8c0cc80c134b16d4937ee6ec9c61a9a
WhiteBear <zxkiki@outlook.kr>
2024-10-29 08:29:24 +0900

    2일차 DP 교육 자료 1ST upload
    교육자료 upload

---
+++ Design Pattern/DAY2/5_Adapter3.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <list>
+#include <vector>
+#include <deque>
+
+// STL 과 Adapter
+// 1. C++ 표준 라이브러리인 STL 에는 이미 list가 있습니다.
+// 2. 그런데, stack 이 필요합니다.
+
+int main()
+{
+	stack<int> s;
+	s.push(10);
+}
